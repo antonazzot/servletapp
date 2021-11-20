f035ba4b959b8d4a72df79a94270cb524f580973
antonazzot <anton.azzot@yandex.ru>
2021-11-20 21:41:31 +0300

    throw in my exception add

--- Service/src/main/java/helperutils/closebaseconnection/JpaUtils.java
+++ Service/src/main/java/helperutils/closebaseconnection/JpaUtils.java
@@ -1,5 +1,6 @@
 package helperutils.closebaseconnection;
 
+import helperutils.MyExceptionUtils.MyJpaException;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.EntityManager;
@@ -9,14 +10,16 @@ public class JpaUtils {
     private JpaUtils() {
     }
 
-    public static void rollBackQuietly(EntityManager em, Exception e) {
+    public static void rollBackQuietly(EntityManager em, Exception e) throws MyJpaException {
         log.error(e.getMessage(), e);
         if (em != null) {
             em.getTransaction().rollback();
         }
+
+            throw new MyJpaException("Null entityM");
     }
 
-    public static void closeQuietly(EntityManager em) {
+    public static void closeQuietly(EntityManager em) throws MyJpaException {
         if (em != null) {
             try {
                 em.close();
@@ -24,5 +27,6 @@ public class JpaUtils {
                 log.error("Close Eror exception = {}", ex.getMessage());
             }
         }
+        throw new MyJpaException("Null entityM");
     }
 }
--- Service/src/main/java/helperutils/closebaseconnection/PostgresSQLUtils.java
+++ Service/src/main/java/helperutils/closebaseconnection/PostgresSQLUtils.java
@@ -1,5 +1,6 @@
 package helperutils.closebaseconnection;
 
+import helperutils.MyExceptionUtils.MySqlException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.ResultSet;
@@ -14,8 +15,9 @@ public class PostgresSQLUtils {
 
     public static void closeQuietly(Statement statement) {
         try {
-            if (statement != null)
-                statement.close();
+            if (statement != null) {
+                statement.close();}
+            throw new MySqlException("Null statement");
         } catch (SQLException e) {
             log.info("Statement close exception= {}", e.getMessage());
             e.printStackTrace();
@@ -24,8 +26,9 @@ public class PostgresSQLUtils {
 
     public static void closeQuietly(ResultSet resultSet) {
         try {
-            if (resultSet != null)
-                resultSet.close();
+            if (resultSet != null) {
+                resultSet.close();}
+            throw new MySqlException("Null ResultSet");
         } catch (SQLException e) {
             log.info("ResultSet close exception= {}", e.getMessage());
             e.printStackTrace();
